a89c9699279f29bc002d4d4d69ca4f1e0ef4571d
Lukas <lukas.wanner@web.de>
2018-10-20 15:19:50 +0200

    first try

--- src/Aufgabe1/testProgramm.java
+++ src/Aufgabe1/testProgramm.java
@@ -8,7 +8,7 @@ public class testProgramm {
         System.out.println("Das sind die Daten vom Eintrag Lukas: " + hd.search("lukas"));
         System.out.println("Das sind die alten Daten von Lukas: " + hd.insert("lukas", "Lukas ist cool"));
         System.out.println("Das sind die neuen Daten von Lukas: " + hd.search("lukas"));
-        System.out.println(hd.insert(1230948, "jawohl !!!!!!!!!!!"));
+        System.out.println(hd.insert(1230948, "jawohl !!!!!!!!!!"));
         System.out.println(hd.search(1230948));
         System.out.println("wir löschen nun eintrag lukas: " + hd.remove("lukas"));
 
