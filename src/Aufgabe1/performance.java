b6f5f274dfdd941364f50e052ca9900b6831d4dc
Lukas <lukas.wanner@web.de>
2018-11-07 13:04:22 +0100

    fast fertig

---
+++ src/Aufgabe1/performance.java
@@ -0,0 +1,52 @@
+package Aufgabe1;
+
+public class performance {
+
+    public long insert1(Dictionary dict) {
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < 8000; i++) {
+            dict.insert(i,i);
+        }
+        long endTime = System.nanoTime();
+        long duration = (endTime -startTime);
+        return duration;
+    }
+
+    public long insert2(Dictionary dict) {
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < 16000; i++) {
+            dict.insert(i,i);
+            System.out.println("...");
+        }
+        long endTime = System.nanoTime();
+        long duration = (endTime -startTime);
+        return duration;
+
+    }
+
+    public long search1(Dictionary dict) {
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < 8000; i++) {
+            dict.search(i);
+        }
+        long endTime = System.nanoTime();
+        long duration = (endTime -startTime);
+        return duration;
+    }
+
+    public long search2(Dictionary dict) {
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < 16000; i++) {
+            dict.search(i);
+        }
+        long endTime = System.nanoTime();
+        long duration = (endTime -startTime);
+        return duration;
+    }
+
+
+}
--- src/Aufgabe1/tui.java
+++ src/Aufgabe1/tui.java
@@ -7,15 +7,17 @@ public class tui {
     public static void tuui() {
         System.out.println("gib was ein : ");
         Scanner in = new Scanner(System.in);
-        Dictionary dict = new SortedArrayDictionary();
+        Dictionary<String,String> dict = new SortedArrayDictionary<>();
         while (true) {
             String input = in.nextLine();
             char first = input.charAt(0);
             String[] a = input.split(" ");
             if(a[0].contains("create") && first == 'c') {
-                if(a[1].equals("Hashdictionary")) {
+                if(a[1].equals("Hash")) {
                     dict = new HashDictionary();
                     System.out.println("verwenden nun HashDictionary");
+                }else{
+                    dict = new SortedArrayDictionary<>();
                 }
             }else if(a[0].contains("read") && first == 'r' && a.length == 3) {
                 dict.insert(a[1],a[2]);
@@ -24,18 +26,24 @@ public class tui {
                 System.out.println("fertig");
                 return;
             }else if(a[0].equals("p")){
-                while (dict.iterator().hasNext()){
-                    System.out.println(dict.iterator().next());
+                for (Dictionary.Entry<String, String> e : dict) {
+                    System.out.println(" KEY: " + e.getKey() + ": " + "VALUE: "+e.getValue());
                 }
             }else if(a[0].equals("s")){
-                Object o = dict.search(a[1]);
-                System.out.println(o.toString());
+                System.out.println(dict.search(a[1]));
             }else if(a[0].equals("r")){
-                dict.remove(a[1]);
-                System.out.println("gelöscht");
+                System.out.println(dict.remove(a[1]));
             }else if(a[0].equals("i")) {
-                dict.insert(a[1],a[2]);
+                System.out.println(dict.insert(a[1],a[2]));
                 System.out.println("erfolgreich eingetragen");
+            }else if(a[0].equals("perf8")){
+                performance perf = new performance();
+                System.out.println("einfügen von 8000 einträgen " + perf.insert1(dict)/1000000 + " ms");
+                System.out.println("suchen nach 8000 einträgen " +perf.search1(dict)/1000000 + " ms");
+            }else if(a[0].equals("perf16")){
+                performance perf = new performance();
+                System.out.println("einfügen von 16000 einträgen " + perf.insert1(dict)/1000000 + " ms");
+                System.out.println("suchen nach 16000 einträgen " +perf.search1(dict)/1000000 + " ms");
             }
 
 
